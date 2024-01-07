6d46bec9111bcd9dbcd1ce913244d4054fb42d37
Nicolas Stebner <nicolas_stebner@hotmail.com>
2024-01-07 15:06:17 -0300

    api-of-render

--- src/lib/port.ts
+++ src/lib/port.ts
@@ -1,9 +1,9 @@
-let API_URL;
+let API_URL = "https://pet-finder-app-3x4r.onrender.com";
 
-if (process.env.NODE_ENV == "production") {
-  API_URL = "";
-} else {
-	API_URL = "http://localhost:3005";
-}
+// if (process.env.NODE_ENV == "production") {
+//   API_URL = "";
+// } else {
+// 	API_URL = "http://localhost:3005";
+// }
 
 export {API_URL}
